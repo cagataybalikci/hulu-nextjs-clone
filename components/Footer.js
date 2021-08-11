06e612cac7da4b4c8c446f81ede303abc10868c4
cagataybalikci <cagataybalikci@gmail.com>
2021-08-11 14:29:23 +0300

    Footer details added. Footer is now responsive.

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,21 @@
 import footerLinks from "../utils/footerHeadings";
 import FooterItem from "./FooterItem";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import InstagramIcon from "@material-ui/icons/Instagram";
 function Footer() {
   return (
     <div className="my-10">
-      <hr className="text-gray-500 my-5 opacity-50" />
-      <div className="grid grid-cols-2 justify-between m-10">
+      <hr className="text-gray-500 my-5 opacity-50 mx-10" />
+      <div className="flex-col  md:grid grid-cols-2 justify-between m-10">
         <div>
           <FooterItem
             title={footerLinks.browse.title}
             items={footerLinks.browse.data}
           />
         </div>
-        <div className="flex justify-between">
+        <div className="md:flex">
           <FooterItem
             title={footerLinks.middle.title}
             items={footerLinks.middle.data}
@@ -26,7 +30,39 @@ function Footer() {
           />
         </div>
       </div>
-      <hr className="text-gray-500 my-5 opacity-50" />
+      <hr className="text-gray-500 my-5 opacity-50 mx-10" />
+      <div className="m-10 flex">
+        <a className="mr-5 cursor-pointer">
+          <FacebookIcon />
+        </a>
+        <a className="mr-5 cursor-pointer">
+          <TwitterIcon />
+        </a>
+        <a className="mr-5 cursor-pointer">
+          <YouTubeIcon />
+        </a>
+        <a className="mr-5 cursor-pointer">
+          <InstagramIcon />
+        </a>
+      </div>
+      <div className="mx-10 text-xs font-light text-gray-500">
+        <a className="mr-5 cursor-pointer hover:text-white">
+          &copy; 2021 Hulu, LLC
+        </a>
+        <a className="mr-5 cursor-pointer hover:text-white">About Ads</a>
+        <a className="mr-5 cursor-pointer hover:text-white">Terms of Use</a>
+        <a className="mr-5 cursor-pointer hover:text-white">Privacy Policy</a>
+        <a className="mr-5 cursor-pointer hover:text-white">
+          Do Not Sell My Personal Information
+        </a>
+        <a className="mr-5 cursor-pointer hover:text-white">
+          Your California Privacy Rights
+        </a>
+        <a className="mr-5 cursor-pointer hover:text-white">
+          TV Parental Guidelines
+        </a>
+        <a className="mr-5 cursor-pointer hover:text-white">Sitemap</a>
+      </div>
     </div>
   );
 }
--- components/FooterItem.js
+++ components/FooterItem.js
@@ -1,8 +1,27 @@
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import React, { useState } from "react";
+
 function FooterItem({ title, items }) {
+  const [show, setShow] = useState(false);
+  const showLinks = () => {
+    setShow(!show);
+  };
   return (
-    <div className="mx-auto">
-      <h1 className="font-md mb-5">{title}</h1>
-      <div className={title === "BROWSE" && "grid grid-cols-4"}>
+    <div className="mx-auto my-5">
+      <div className="flex justify-between">
+        <h1 className="font-md mb-5">{title}</h1>
+        <span
+          className="flex cursor-pointer hover:text-white md:hidden"
+          onClick={showLinks}
+        >
+          <KeyboardArrowDownIcon />
+        </span>
+      </div>
+      <div
+        className={`${!show && "hidden"}${
+          title === "BROWSE" ? " md:grid grid-cols-4" : " md:grid"
+        }`}
+      >
         {items.map((item) => (
           <p
             className="mb-1 text-gray-500 cursor-pointer hover:text-white hover:scale-105"
